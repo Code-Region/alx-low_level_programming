91972e957547fb7d14774dafe94d8e8f48ca2cd2
Code-Region <ishaqibrahim4real2020@gmail.com>
2022-10-13 09:19:25 -0700

    last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -7,7 +8,6 @@
 
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, str1, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 59);
 	return (1);
 }
