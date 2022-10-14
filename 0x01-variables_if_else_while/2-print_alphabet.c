0b536a58a6a8cd202c08fcada7280b51bdd9b824
Code-Region <ishaqibrahim4real2020@gmail.com>
2022-10-14 08:23:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ */
+int main(void)
+{
+	char aph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alh[i]);
+	}
+	putchar("\n");
+	return (0);
+}
