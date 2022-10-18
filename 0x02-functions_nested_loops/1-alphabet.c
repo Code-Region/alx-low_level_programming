ace934f94c8465c9351bbc16f7696bea5f3739a6
Code-Region <ishaqibrahim4real2020@gmail.com>
2022-10-18 05:04:03 -0700

    tas 1 /alx-low_level_programming/0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  print all alphatbet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; letter <= 'z')
+	{
+		_putchar(alphabet)
+		alphabet++;
+	}
+}
+_putchar('\n');
