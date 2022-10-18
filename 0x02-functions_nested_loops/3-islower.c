93250e75d4e91175947fca8a7056f541a4d84bb5
Code-Region <ishaqibrahim4real2020@gmail.com>
2022-10-18 07:03:49 -0700

    tast 3 cherk for smalll letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - check for small letters.
+ * Return: 1 if c is lower and 0 other wise
+ */
+
+int _islower(int c)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z'; lett ++)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n');
+}
