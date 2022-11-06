ac00076717d94b66ac90ed69ca5929c126535f2a
Code-Region <ishaqibrahim4real2020@gmail.com>
2022-11-06 07:26:54 -0800

    test mode

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * main - print putchar
- * Return: 0 success
+ * _putchar - my write
+ * @c: input
+ * Return: c
  */
 
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	return (write(1, &c, 1));
 }
